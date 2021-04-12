e2cb40c290385eb0cfd5816aaa76df8d1f1c3482
cutehaddy <76763808+cutehaddy@users.noreply.github.com>
2021-04-13 00:54:02 +0100

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,50 @@
+zendaya = {
+    'name':'zendaya',
+    'image':'imgs/14.jpg',
+    'last-msg':'hey whats up.;2:00pm',
+    'reciept':'sent',
+}
+
+breezy = {
+    'name':'breezy',
+    'image':'imgs/breezy.jpg',
+    'last-msg':'just dropped a single, check it out http:www.blahblah.com/breezy;1:20pm',
+    'reciept':'seen',
+}
+
+davido = {
+    'name':'davido',
+    'image':'imgs/davido.jpg',
+    'last-msg':'obo how is the party going;1:11pm',
+    'reciept':'unread',
+}
+
+rihanna = {
+    'name':'rihanna',
+    'image':'imgs/rihanna.png',
+    'last-msg':'thanks;1:01pm',
+    'reciept':'read',
+}
+
+weeknd = {
+    'name':'weeknd',
+    'image':'imgs/weeknd.jpg',
+    'last-msg':'i dont know whats up with her.... :/;12:59pm',
+    'reciept':'sent',
+}
+
+wizzy = {
+    'name':'wizzy',
+    'image':'imgs/wizkid2.jpg',
+    'last-msg':'have you loaded the stuffs;9:00am',
+    'reciept':'seen',
+}
+
+jordan = {
+    'name':'jordan',
+    'image':'imgs/jordan.jpg',
+    'last-msg':'i guess i would just do it ma self;2:00am',
+    'reciept':'seen',
+}
+
+users = [zendaya, breezy, davido, rihanna, weeknd, wizzy, jordan]
